9e941128cb830d5657bccfe035dbc1836738d6db
Bhavesh Rai <113577193+raibhavesh@users.noreply.github.com>
2024-04-28 00:41:56 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,14 @@
+public class variables{
+    public static void main(String[] args){
+        int myNumber =865;
+
+        int yourNumber;
+        yourNumber = 865;
+        System.out.println(yourNumber);
+        yourNumber = 45;
+        System.out.println(yourNumber);
+
+
+    }
+
+}

c2563d73aa91515df3dc77cea32407337ce852cc
raibhavesh <113577193+raibhavesh@users.noreply.github.com>
2024-07-28 23:14:37 +0530

    making button using switch and break

---
+++ switchebreak.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class switchebreak{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+
+        switch(button){
+            case 1 : System.out.println("hello");
+            break;
+            case 2: System.out.println("namaste");
+            break;
+            case 3 :System.out.println("bonjour");
+            break;
+            default : System.out.println("invalid");
+        }
+    }
+}

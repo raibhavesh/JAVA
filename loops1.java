88e32842577f7313f5a0f0326a39c162a1f0c6fa
raibhavesh <113577193+raibhavesh@users.noreply.github.com>
2024-07-28 23:47:14 +0530

    java programming using for loop

---
+++ loops1.java
@@ -0,0 +1,11 @@
+
+public class loops1 {
+    public static void main(String args[]){
+        for(int count = 0;count<50; count = count + 1 ){
+            System.out.println("namaste india");
+
+        }
+
+    }
+    
+}

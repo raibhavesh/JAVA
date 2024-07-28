b6bb135a0f661f8cd7ba41efdcc82369b8b9e1e4
raibhavesh <113577193+raibhavesh@users.noreply.github.com>
2024-07-28 23:07:55 +0530

    using switches

---
+++ switches.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class switches{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+
+        if(button==1){
+        System.out.println("Hello");
+        }
+        else if(button ==2){
+            System.out.println("Namaste");
+        }
+        else if(button == 3){
+            System.out.println("Namaste");
+        }
+        else{
+            System.out.println("Bonjour");
+        }
+
+}
+
+    
+}

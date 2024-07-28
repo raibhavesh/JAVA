d3b8137519e155f4c5004fd10393ad5c41b3fda9
raibhavesh <113577193+raibhavesh@users.noreply.github.com>
2024-07-28 22:51:11 +0530

    to find weather entered no. is odd or even

---
+++ oddeven.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class oddeven {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        
+
+        if(number%2 == 0){
+            System.out.println("the number is even");
+
+
+    }
+    else{
+        System.out.println("the number enter is odd");
+    }
+}
+}

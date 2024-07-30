ce18fd258f55cb77ae94c87a8ecccb4ae706e70e
raibhavesh <113577193+raibhavesh@users.noreply.github.com>
2024-07-31 00:30:20 +0530

    pyramid pattern with number

---
+++ pattern7.java
@@ -0,0 +1,15 @@
+public class pattern7 {
+    public static void main(String args[]){
+        for(int i=1; i<=5;i++){
+            for(int j=1;j<=5-i;j++){
+                System.out.print(" ");
+            }
+         for(int j=1; j<=i;j++){
+            System.out.print(i+" ");
+
+         }   
+         System.out.println();
+        }
+    }
+    
+}
